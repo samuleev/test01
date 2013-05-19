0f7d419ce702f61a14c35c01bb8f0a351f05349e
Andrey Samuleev <andrey@samuleev.com>
2013-05-19 22:37:17 +0300

    wqe

---
+++ App.java
@@ -0,0 +1,13 @@
+package testpak.MavenTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello branch World!" );
+    }
+}
